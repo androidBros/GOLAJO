30dcc3e34418822703dc1bdf2e578ff63657bdb1
theo-no <tjsgh1512@naver.com>
2022-06-28 02:22:56 +0900

    20220408 - 56

--- app/src/androidTest/java/com/chadong/lucky/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/chadong/lucky/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.lucky", appContext.getPackageName());
+        assertEquals("com.chadong.lucky", appContext.getPackageName());
     }
 }
